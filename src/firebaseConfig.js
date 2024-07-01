a323a0ea7dac18587e4ccbd004e60d816ba26956
Papito <papinifc@gmail.com>
2024-07-01 08:26:37 -0300

    service account with dotenv

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const admin = require('firebase-admin');
-const path = require('path');
 
-const serviceAccount = require(path.join(__dirname, 'secrets/serviceAccountKey.json'));
+// Parse the service account key from the environment variable
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
